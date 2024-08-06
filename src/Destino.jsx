8b6e69c619923a789341c72f7d7310bb63b48671
NICOLAS ANDRÉ KRAUSE DE MELLO <nicolas_a_mello@estudante.sesisenai.org.br>
2024-08-06 08:56:39 -0300

    Calcula e exibe distância até o destino

--- src/Destino.jsx
+++ src/Destino.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 
-const Destino = ({ nome, descricao }) => {
-  return (
-    <div className="destino">
-      <h2>{nome}</h2>
-      <p>{descricao}</p>
-    </div>
-  );
-};
-
-export default Destino;git add .
+ const Destino = ({ nome, descricao, distancia }) => {
+    return (
+      <div className="destino">
+        <h2>{nome}</h2>
+        <p>{descricao}</p>
+      <p>Distância: {distancia} km</p>
+      </div>
+    );
+  };
+  
+  export default Destino;
